6dc5916df0acb48892d9359f370e0ac5a1d2e2cb
Sergio Azizi <sergio.azizi.14@gmail.com>
2021-08-16 21:40:48 +0100

    feat: integrated socketIO with threejs client, implemented basic controls, implemented connecting and disconnecting user

--- client/client.js
+++ client/client.js
@@ -1,30 +1,71 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GUI } from "three/examples/jsm/libs/dat.gui.module";
-import * as CANNON from "cannon-es";
+
+let myId = "";
+let updateInterval;
+const users = {};
+
+const map = {};
+document.onkeydown = document.onkeyup = (e) => {
+  if (
+    e.key === "ArrowUp" ||
+    e.key === "ArrowRight" ||
+    e.key === "ArrowDown" ||
+    e.key === "ArrowLeft"
+  )
+    map[e.key] = e.type === "keydown";
+};
 
 /**
  * add socketIO
  */
 const socket = io("http://localhost:3001");
-socket.on("connection", () => {
-  console.log("new user entered the game");
+socket.on("connect", () => {
+  console.log("connect");
+});
+socket.on("joined", (id, activeUsers) => {
+  myId = id;
+  console.log(activeUsers);
+  for (const [userId, userData] of Object.entries(activeUsers)) {
+    users[userId] = new THREE.Mesh(
+      new THREE.SphereGeometry(1),
+      new THREE.MeshStandardMaterial({ color: 0xff0000 })
+    );
+    users[userId].name = userData.username;
+    users[userId].position.copy(userData.position);
+    scene.add(users[userId]);
+  }
+  updateInterval = setInterval(() => {
+    socket.emit("update", map);
+  }, 50);
+});
+socket.on("add new user", (id, newUser) => {
+  users[id] = new THREE.Mesh(
+    new THREE.SphereGeometry(1),
+    new THREE.MeshStandardMaterial({ color: 0xff0000 })
+  );
+  users[id].name = newUser.username;
+  users[id].position.copy(newUser.position);
+  scene.add(users[id]);
+});
+socket.on("update", (activeUsers) => {
+  for (const [userId, userData] of Object.entries(activeUsers)) {
+    if (users[userId]) {
+      users[userId].position.copy(userData.position);
+      users[userId].quaternion.copy(userData.quaternion);
+    }
+  }
 });
 
-const connectNewUser = () => {
-  const username = prompt("what's your name?");
-  socket.emit("new user", username);
-};
+socket.on("removePlayer", (id) => {
+  console.log(scene);
+  scene.remove(scene.getObjectByName(users[id].name));
+  // clearInterval(updateInterval);
+  delete users[id];
+});
 
-/**
- * Debug
- */
-const parameters = {
-  force: 100,
-};
-const gui = new GUI();
-gui.add(parameters, "force").min(0).max(1000).step(1);
-
+//
 const canvas = document.querySelector("#canvas");
 
 const scene = new THREE.Scene();
@@ -65,9 +106,9 @@ const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
-  100
+  1000
 );
-camera.position.set(0, 3, 3);
+camera.position.set(0, 6, 6);
 scene.add(camera);
 
 // Controls
@@ -83,13 +124,32 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Physics
- */
+// ground
+const ground = new THREE.Mesh(
+  new THREE.PlaneGeometry(100, 100),
+  new THREE.MeshStandardMaterial({ color: 0x222222 })
+);
+ground.rotateX(-Math.PI / 2);
+scene.add(ground);
+
+const tick = () => {
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+tick();
+
+/*
+//Physics
 const world = new CANNON.World();
 world.broadphase = new CANNON.SAPBroadphase(world);
 world.gravity.set(0, -9.81, 0);
-// world.allowSleep = true;
+world.allowSleep = true;
 const groundMaterial = new CANNON.Material("groundMaterial");
 const userMaterial = new CANNON.Material("userMaterial");
 const userGroundContactMaterial = new CANNON.ContactMaterial(
@@ -103,15 +163,6 @@ const userGroundContactMaterial = new CANNON.ContactMaterial(
 );
 world.addContactMaterial(userGroundContactMaterial);
 
-// floor mesh
-const floorMesh = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshStandardMaterial()
-);
-floorMesh.receiveShadow = true;
-floorMesh.rotation.x = -Math.PI * 0.5;
-floorMesh.position.y = -0.5;
-scene.add(floorMesh);
 
 // floor body
 const floorOrientation = floorMesh.quaternion;
@@ -127,27 +178,33 @@ const floorBody = new CANNON.Body({
   ),
 });
 // floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+floorBody.position.copy(floorMesh.position);
 world.addBody(floorBody);
 
 // user mesh
-const userMesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshNormalMaterial()
-);
+// box
+// const userMesh = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshNormalMaterial()
+// );
+// spehre
 // const userMesh = new THREE.Mesh(
 //   new THREE.SphereGeometry(1),
 //   new THREE.MeshNormalMaterial()
 // );
-userMesh.position.y = 1;
-scene.add(userMesh);
+//
+// userMesh.position.y = 1;
+// scene.add(userMesh);
 
 // user body
-const userBody = new CANNON.Body({
-  mass: 1.0,
-  position: new CANNON.Vec3(0, 3, 0),
-  shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
-  material: userMaterial,
-});
+// box
+// const userBody = new CANNON.Body({
+//   mass: 1.0,
+//   position: new CANNON.Vec3(0, 3, 0),
+//   shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
+//   material: userMaterial,
+// });
+// sphere
 // const userBody = new CANNON.Body({
 //   mass: 1.0,
 //   position: new CANNON.Vec3(0, 1, 0),
@@ -156,68 +213,35 @@ const userBody = new CANNON.Body({
 //   linearDamping: 0.6,
 //   angularDamping: 0.6,
 // });
-userBody.position.copy(userMesh.position);
-world.addBody(userBody);
+//
+// userBody.position.copy(userMesh.position);
+// world.addBody(userBody);
+
+let userMesh = null;
+let userBody = null;
+const userGeometry = new THREE.BoxGeometry(1, 1, 1);
+const userMeshMaterial = new THREE.MeshNormalMaterial();
+const userShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
+
+const users = {};
+const createUser = () => {
+  const pos = [
+    Math.sin((Math.random() - 0.5) * 2 * Math.PI) * 5,
+    3,
+    Math.cos((Math.random() - 0.5) * 2 * Math.PI) * 5,
+  ];
+  userMesh = new THREE.Mesh(userGeometry, userMeshMaterial);
+  userMesh.position.set(...pos);
+  scene.add(userMesh);
+  userBody = new CANNON.Body({
+    mass: 1.0,
+    position: new CANNON.Vec3(...pos),
+    shape: userShape,
+    material: userMaterial,
+  });
+  userBody.position.copy(userMesh.position);
+  world.addBody(userBody);
+};
 
 // userBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0));
-
-/**
- * Animate
- */
-const map = {};
-const navigate = (event) => {
-  const direction = event.key;
-  map[direction] = event.type === "keydown";
-  const force = parameters.force;
-  for (const [key, value] of Object.entries(map)) {
-    if (value && key === "ArrowUp") {
-      userBody.applyForce(
-        new CANNON.Vec3(0, 0, -force),
-        new CANNON.Vec3(0, 0, 0)
-      );
-    }
-    if (value && key === "ArrowRight") {
-      userBody.applyForce(
-        new CANNON.Vec3(force, 0, 0),
-        new CANNON.Vec3(0, 0, 0)
-      );
-    }
-    if (value && key === "ArrowDown") {
-      userBody.applyForce(
-        new CANNON.Vec3(0, 0, force),
-        new CANNON.Vec3(0, 0, 0)
-      );
-    }
-    if (value && key === "ArrowLeft") {
-      userBody.applyForce(
-        new CANNON.Vec3(-force, 0, 0),
-        new CANNON.Vec3(0, 0, 0)
-      );
-    }
-  }
-};
-document.onkeydown = document.onkeyup = navigate;
-
-const clock = new THREE.Clock();
-let oldElapsedTime = 0;
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - oldElapsedTime;
-  oldElapsedTime = elapsedTime;
-
-  // update Physics world
-  world.step(1 / 60, deltaTime, 3);
-
-  userMesh.position.copy(userBody.position);
-  userMesh.quaternion.copy(userBody.quaternion);
-
-  // Update controls
-  controls.update();
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-tick();
+*/
