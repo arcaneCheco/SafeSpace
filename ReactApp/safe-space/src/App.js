793f94d82c9f77622882545893b9ea65c74ab887
Daniel Slattery <daniel.slattery1@gmail.com>
2021-08-17 16:04:03 +0100

    created basic 3D world for UI development

--- ReactApp/safe-space/src/App.js
+++ ReactApp/safe-space/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
-import Container3D from "./components3D/Container3D";
+import Container3D from "./components3D/Container3D/Container3D";
+import VideoContainer from "./componentsUI/VideoContainer/VideoContainer";
 
 
 export default function App() {
   return (
-    <Container3D />
+    <div>
+      <Container3D />
+      <VideoContainer />
+    </div>
   );
 }
---
+++ ReactApp/safe-space/src/componentsUI/VideoContainer/VideoContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoContainer.css'
+
+const VideoContainer = () => {
+  return (
+    <div className='VideoContainer'>
+
+    </div>
+  )
+}
+
+export default VideoContainer
