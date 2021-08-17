284e0a4c0d2559dcb6479380565d3a3b2b935d84
Daniel Slattery <daniel.slattery1@gmail.com>
2021-08-17 22:42:22 +0100

    removed obsolete three fibre folders and components

--- ReactApp/safe-space/src/components/VideoContainer/VideoContainer.jsx
+++ ReactApp/safe-space/src/components/VideoContainer/VideoContainer.jsx
@@ -4,7 +4,7 @@ import './VideoContainer.css'
 const VideoContainer = () => {
   return (
     <div className='VideoContainer'>
-      <h1 style={{textAlign: 'center', margin: 0, padding: 100, color: 'gray'}}>A nice spot to put some Web RTC up possibly</h1>
+      <h1 style={{textAlign: 'center', margin: 0, padding: 100, color: 'gray'}}>A nice spot to put some Web RTC up possibly 🌈🚀</h1>
 
     </div>
   )
