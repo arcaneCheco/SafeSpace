bf8cf399622394460932cb28b2925fb59fa35249
Daniel Slattery <daniel.slattery1@gmail.com>
2021-08-18 13:29:21 +0100

    update for merge

--- ReactApp/safe-space/src/aWonderfulWorld.js
+++ ReactApp/safe-space/src/aWonderfulWorld.js
@@ -1,81 +1,123 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GUI } from "three/examples/jsm/libs/dat.gui.module";
+import * as dat from "dat.gui";
+
 
 const aWonderfulWorld = (scene, canvas) => {
+  // Debug
+  const gui = new dat.GUI();
+
+  /**
+   * Textures
+   */
+  const textureLoader = new THREE.TextureLoader();
+
+  const grassColorTexture = textureLoader.load('/textures/grass/color.jpg');
+  const grassAmbientOcclusionTexture = textureLoader.load(
+    "/textures/grass/ambientOcclusion.jpg"
+  );
+  const grassNormalTexture = textureLoader.load("/textures/grass/normal.jpg");
+  const grassRoughnessTexture = textureLoader.load(
+    "/textures/grass/roughness.jpg"
+  );
+
+  grassColorTexture.repeat.set(8, 8);
+  grassAmbientOcclusionTexture.repeat.set(8, 8);
+  grassNormalTexture.repeat.set(8, 8);
+  grassRoughnessTexture.repeat.set(8, 8);
+
+  grassColorTexture.wrapS = THREE.RepeatWrapping;
+  grassAmbientOcclusionTexture.wrapS = THREE.RepeatWrapping;
+  grassNormalTexture.wrapS = THREE.RepeatWrapping;
+  grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
+
+  grassColorTexture.wrapT = THREE.RepeatWrapping;
+  grassAmbientOcclusionTexture.wrapT = THREE.RepeatWrapping;
+  grassNormalTexture.wrapT = THREE.RepeatWrapping;
+  grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
   /**
    * Lights
    */
-   const ambientLight = new THREE.AmbientLight();
-   scene.add(ambientLight);
+   const ambientLight = new THREE.AmbientLight('#b9d5ff', 1.5)
+   gui.add(ambientLight, 'intensity').min(1).max(3).step(0.001)
+   scene.add(ambientLight)
 
-   /**
-    * Sizes
-    */
-   const sizes = {
-     width: window.innerWidth,
-     height: window.innerHeight,
-   };
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
 
-   window.addEventListener("resize", () => {
-     // Update sizes
-     sizes.width = window.innerWidth;
-     sizes.height = window.innerHeight;
+  window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-     // Update camera
-     camera.aspect = sizes.width / sizes.height;
-     camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
-     // Update renderer
-     renderer.setSize(sizes.width, sizes.height);
-     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-   });
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
 
-   /**
-    * Camera
-    */
-   // Base camera
-   const camera = new THREE.PerspectiveCamera(
-     75,
-     sizes.width / sizes.height,
-     0.1,
-     1000
-   );
-   camera.position.set(0, 6, 6);
-   scene.add(camera);
+  /**
+   * Camera
+   */
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    1000
+  );
+  camera.position.set(0, 30, 60);
+  scene.add(camera);
 
-   // Controls
-   const controls = new OrbitControls(camera, canvas);
-   controls.enableDamping = true;
+  // Controls
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
 
-   /**
-    * Renderer
-    */
-   const renderer = new THREE.WebGLRenderer({ canvas });
-   renderer.shadowMap.enabled = true;
-   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-   renderer.setSize(sizes.width, sizes.height);
-   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  /**
+   * Renderer
+   */
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-   // ground
-   const ground = new THREE.Mesh(
-     new THREE.PlaneGeometry(100, 100),
-     new THREE.MeshStandardMaterial({ color: 0x222222 })
-   );
-   ground.rotateX(-Math.PI / 2);
-   scene.add(ground);
+ // Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(200, 200),
+  new THREE.MeshStandardMaterial({
+      map: grassColorTexture,
+      aoMap: grassAmbientOcclusionTexture,
+      normalMap: grassNormalTexture,
+      roughnessMap: grassRoughnessTexture
+  })
+)
+floor.geometry.setAttribute(
+  'uv2',
+  new THREE.Float32BufferAttribute(floor.geometry.attributes.uv.array, 2)
+)
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
 
-   const tick = () => {
-     // Update controls
-     controls.update();
+  const tick = () => {
+    // Update controls
+    controls.update();
 
-     // Render
-     renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera);
 
-     // Call tick again on the next frame
-     window.requestAnimationFrame(tick);
-   };
-   tick();
-}
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+  };
+  tick();
+};
 
-export {aWonderfulWorld as aWonderfulWorld}
+export { aWonderfulWorld as aWonderfulWorld };
